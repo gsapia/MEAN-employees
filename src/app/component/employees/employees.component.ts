b540a54fb3f9e072495735a71511a1c543f8ad28
gsapia <guillermosapia.lsp@hotmail.com.ar>
2020-02-23 15:15:00 -0300

    first commi

---
+++ src/app/component/employees/employees.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+import { NgForm } from '@angular/forms';
+import { Employee } from 'src/app/models/employee';
+
+declare var M: any;
+ 
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css'],
+  providers: [EmployeeService]
+})
+export class EmployeesComponent implements OnInit {
+
+  constructor(public employeeService: EmployeeService) { 
+
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+  
+  getEmployees(){
+  this.employeeService.getEmployees()
+  .subscribe(res => {
+       this.employeeService.employees = res as Employee[];
+  });
+  }
+
+  addEmployee(form: NgForm){
+    if(form.value._id){
+      this.employeeService.putEmployee(form.value)
+       .subscribe(res => {
+        this.resetForm(form);
+        M.toast({html: 'Update Successfully !'});
+        this.getEmployees();
+       })
+    }else {
+      this.employeeService.postEmployee(form.value)
+      .subscribe(res => {
+        this.resetForm(form);
+        M.toast({html: 'Save Successfully !'});
+        this.getEmployees();
+      });
+    }
+    
+  }
+
+  resetForm(form?: NgForm){
+    if (form){
+      form.reset();
+      this.employeeService.selectedEmployee = new Employee();
+    }
+  }
+
+   editEmployee(employee: Employee){
+    this.employeeService.selectedEmployee = employee;
+   }
+
+   deleteEmployee(_id: string){
+    if(confirm('Are your sure you want to delete it ?'))
+    {
+    this.employeeService.deleteEmployee(_id)
+    .subscribe(res => { this.getEmployees(); 
+      M.toast({html:'Delete successfully !'})
+    })      
+    }
+  
+   }
+
+}
